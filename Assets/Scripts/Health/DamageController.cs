9e99697d667f25afaabcec5949c0cea245fd7625
Panahda <92427609+Panahda@users.noreply.github.com>
2024-02-03 00:33:16 +0800

    Update: Gameplay and visuals
    - Update lighting and environment
    - Added health and health UI
    - added enemy
    - Added enemy patrol state (bugged)
    - imported assests

---
+++ Assets/Scripts/Health/DamageController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    [SerializeField] private float bombDamage = 10.0f;
+
+    [SerializeField] private GameObject explosiveParticle = null;
+
+    [SerializeField] private HealthController _healthController = null;
+
+    [SerializeField] private AudioClip bombAudio = null;
+    private bool playingAudio;
+    private AudioSource bombAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bombAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            explosiveParticle.SetActive(true);
+            bombAudioSource.PlayOneShot(bombAudio);
+            _healthController.currentPlayerHealth -= bombDamage;
+            _healthController.TakeDamage();
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            playingAudio = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(playingAudio)
+        {
+            if (bombAudioSource.isPlaying)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Health/HealthController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    [Header("Player Health Amount")]
+    public float currentPlayerHealth = 100.0f;
+    [SerializeField] private float maxPLayerHealth = 100.0f;
+    [SerializeField] private int regenRate = 1;
+    private bool canRegen = false;
+
+    [Header("Add the splatter image here")]
+    [SerializeField] private Image redSplatterImage = null;
+
+    [Header("Hurt Image Flash")]
+    [SerializeField] private Image hurtImage = null;
+    [SerializeField] private float hurtTimer = 0.1f;
+
+    [Header("HealTimer")]
+    [SerializeField] private float healCooldown = 3.0f;
+    [SerializeField] private float maxHealCooldown = 3.0f;
+    [SerializeField] private bool startCooldown = false;
+
+    [Header("Audio Name")]
+    [SerializeField] private AudioClip hurtAudio = null;
+    private AudioSource healthAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void UpdateHealth()
+    {
+        Color splatterAlpha = redSplatterImage.color;
+        splatterAlpha.a = 1 - (currentPlayerHealth / maxPLayerHealth);
+        redSplatterImage.color = splatterAlpha;
+    }
+
+    IEnumerator HurtFlash()
+    {
+        hurtImage.enabled = true;
+        healthAudioSource.PlayOneShot(hurtAudio);
+        yield return new WaitForSeconds(hurtTimer);
+        hurtImage.enabled = false;
+    }
+
+    public void TakeDamage()
+    {
+        if(currentPlayerHealth >= 0)
+        {
+            canRegen = false;
+            StartCoroutine(HurtFlash());
+            UpdateHealth();
+            healCooldown = maxHealCooldown;
+            startCooldown = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (startCooldown)
+        {
+            healCooldown -= Time.deltaTime;
+            if(healCooldown <= 0)
+            {
+                canRegen = true;
+                startCooldown = false;
+            }
+        }
+
+        if (canRegen)
+        {
+            if(currentPlayerHealth <= maxPLayerHealth - 0.01)
+            {
+                currentPlayerHealth += Time.deltaTime * regenRate;
+                UpdateHealth();
+            }
+            else
+            {
+                currentPlayerHealth = maxPLayerHealth;
+                healCooldown = maxHealCooldown;
+                canRegen = false;
+            }
+        }
+    }
+}
+ 
